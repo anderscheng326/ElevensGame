311d081230d8e4dc9c42961b4ce053b512ef7375
anderscheng326 <anderscheng326@gmail.com>
2025-03-12 21:52:44 -0400

    add class skeleton/implement deal method

--- Elevens.cs
+++ Elevens.cs
@@ -23,3 +23,78 @@ Class: ElevensGame
    +Elevens(hand:List<Card>):void
 */
 
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ElevensGame
+{
+
+//fields
+List<Card> hand = new List<Card>();
+bool isGameOver = false;
+
+//properties
+public List<Card> Hand
+{
+   get {return hand;}
+}
+
+public List<int> SelectCard 
+{
+   get; set;
+}
+
+public ElevensGame(List<Card> hand)
+{
+   this.hand = hand;
+   this.isGameOver = false;
+}
+
+public List<Card> Deal()
+{
+   //to be implemented
+   Deck deck = new Deck();
+   deck.Shuffle();
+   for(int x = 0; x<9; x++)
+   {
+      hand[x] = deck.TakeTopCard();
+   }
+   return hand;
+}
+
+public void PlayerMove()
+{
+   //to be implemented
+}
+
+public bool SystemCheck()
+{
+   //to be implemented
+   return true;
+}
+
+public Card Replace()
+{
+   //to be implemented
+   return newcard; //placeholder
+}
+
+public void Reset()
+{
+   //to be implemented
+}
+
+public bool CheckWin()
+{
+   //to be implemented
+   return isGameOver;
+}
+
+
+}
+
