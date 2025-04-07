3595fd542748bf60ed4fe55cd69da1db56da86e8
anderscheng326 <anderscheng326@gmail.com>
2025-04-07 18:43:12 -0400

    update functions and implement main program

--- Elevens.cs
+++ Elevens.cs
@@ -34,95 +34,150 @@ using System.Threading.Tasks;
 public class ElevensGame
 {
 
-//fields
-List<Card> hand = new List<Card>();
-bool isGameOver = false;
+   //fields
+   List<Card> hand = new List<Card>();
+   bool isGameOver = false;
 
-private static Deck gameDeck = new Deck(); //static shared deck
+   private static Deck gameDeck = new Deck(); //static shared deck
 
-//properties
-public List<Card> Hand
-{
-   get {return hand;}
-}
-
-public List<int> SelectCard 
-{
-   get; set;
-}
-
-public ElevensGame(List<Card> hand)
-{
-   this.hand = hand;
-   this.isGameOver = false;
-}
-
-public List<Card> Deal()
-{
-   gameDeck.Shuffle();
-   for(int x = 0; x<9; x++)
+   //properties
+   public List<Card> Hand
    {
-      hand[x] = gameDeck.TakeTopCard();
+      get {return hand;}
    }
-   return hand;
-}
 
-public void PlayerMove()
-{
-   Console.WriteLine("Select cards by index separated by space: ");
-   string input = Console.ReadLine();
-   SelectCard = input.Split(' ').Select(int.Parse).ToList();
-
-   if (SelectCard.Count > 0 && SystemCheck())
+   public List<int> SelectCard 
    {
-      foreach (int index in SelectCard)
+      get; set;
+   }
+
+   public ElevensGame(List<Card> hand)
+   {
+      this.hand = hand;
+      this.isGameOver = false;
+   }
+
+   public List<Card> Deal()
+   {
+      gameDeck.Shuffle();
+      for(int x = 0; x<9; x++)
       {
-         Replace(index);
+         hand[x] = gameDeck.TakeTopCard();
       }
-      Console.WriteLine("Match made!");
+      return hand;
    }
-   else
+
+   public void PlayerMove()
    {
-      Console.WriteLine("Invalid Move. Try again.");
-   }
-}
+      Console.WriteLine("Select cards by index separated by space: ");
+      string input = Console.ReadLine();
+      SelectCard = input.Split(' ').Select(int.Parse).ToList();
 
-public bool SystemCheck()
-{
-   int sum = 0;
-   foreach (int index in SelectCard)
+      if (SelectCard.Count > 0 && SystemCheck())
+      {
+         foreach (int index in SelectCard)
+         {
+            Replace(index);
+         }
+         Console.WriteLine("Match made!");
+      }
+      else
+      {
+         Console.WriteLine("Invalid Move. Try again.");
+      }
+   }
+
+   public bool SystemCheck()
    {
-      sum += (int)hand[index].Rank;
+      if (SelectCard.Count == 2) //check for 2 card adding up to 11
+      {
+         int sum = (int)hand[SelectCard[0]].Rank + (int)hand[SelectCard[1]].Rank;
+         if (sum == 11)
+         {
+            return true;
+         }
+      }
+      if (SelectCard.Count == 3) //check for 3 face cards
+      {
+        List<Rank> selectedRanks = SelectCard.Select(index => hand[index].Rank).ToList();
+        //list of ranks of selected cards
+
+         bool hasJack = selectedRanks.Contains(Rank.Jack);
+         bool hasQueen = selectedRanks.Contains(Rank.Queen);
+         bool hasKing = selectedRanks.Contains(Rank.King);
+         //bools for checking if theres one Jack, Queen, King
+
+         if (hasJack && hasQueen && hasKing)
+         {
+            return true;
+         }
+      }
+      return false;
    }
-   return (sum == 11 || sum == 36 );  //11 for pairs, 36 for triple face for now
-}
 
-public Card Replace(int index)
-{
-   Card newCard = gameDeck.TakeTopCard();
-   return newCard;
-}
-
-public void Reset()
-{
-   gameDeck = new Deck();
-   gameDeck.Shuffle();
-
-   hand.Clear();
-   for(int x = 0; x<9; x++)
+   public Card Replace(int index)
    {
-      hand[x] = gameDeck.TakeTopCard();
+      Card newCard = gameDeck.TakeTopCard();
+      hand[index] = newCard;
+      return newCard;
    }
-   
-   isGameOver = false;
-   SelectCard = new List<int>();
-}
 
-public bool CheckWin()
-{
-   //to be implemented
-   return isGameOver;
-}
+   public void Reset()
+   {
+      gameDeck = new Deck();
+      gameDeck.Shuffle();
+
+      hand.Clear();
+      for(int x = 0; x<9; x++)
+      {
+         hand[x] = gameDeck.TakeTopCard();
+      }
+      
+      isGameOver = false;
+      SelectCard = new List<int>();
+   }
+
+   public bool CheckWin()
+   {
+      if (gameDeck.Cards.Count == 0) //check if deck is empty
+      {
+         isGameOver = true; 
+         Console.WriteLine("Congratulations! You've Won! No cards left in deck!");
+         return true; //game is won
+      }
+
+      for(int i = 0; i < hand.Count; i++) //check for valid 2 card moves
+      {
+         for (int j = i + 1; j < hand.Count; j++)
+         {
+            if ((int)hand[i].Rank + (int)hand[j].Rank == 11) //if any cards add up to 11
+            {
+               return false; //valid move exists, game not over
+            }
+         }
+      } 
+
+      for(int i = 0; i < hand.Count; i++) //check for valid 3 card moves
+      {
+         for (int j = i + 1; j < hand.Count; j++)
+         {
+            for (int k = j + 1; k < hand.Count; k++)
+            {
+               bool hasJack = hand[i].Rank == Rank.Jack || hand[j].Rank == Rank.Jack || hand[k].Rank == Rank.Jack;
+               bool hasQueen = hand[i].Rank == Rank.Queen || hand[j].Rank == Rank.Queen || hand[k].Rank == Rank.Queen;
+               bool hasKing = hand[i].Rank == Rank.King || hand[j].Rank == Rank.King || hand[k].Rank == Rank.King;
+               
+               if (hasJack && hasQueen && hasKing) //if has all 3 faces 
+               {
+                  return false; //valid move exists, game not over
+               }
+            }
+         }
+      } 
+      isGameOver=true; 
+      Console.WriteLine("Game Over! No valid moves remain.");
+      return true; //game is lost
+   }
 
 
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Welcome to Elevens Card Game!");
+
+        List<Card> initialHand = new List<Card>(new Card[9]); // intizialize game with card capacity of 9
+        ElevensGame game = new ElevensGame(initialHand);
+
+        Console.WriteLine("Dealing cards...");
+        game.Deal(); //start game by dealing intial hand 
+        Console.WriteLine("Your current hand:");
+        DisplayHand(game.Hand); //display hand (static method implemented at end)
+
+        while (!game.CheckWin())  //main gameplay loop - playermove, displayhand, checkwin
+        {
+            Console.WriteLine("\nIt's your turn to play.");
+            game.PlayerMove();
+
+            Console.WriteLine("Your updated hand:");
+            DisplayHand(game.Hand);
+
+            if (game.CheckWin())
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Thank you for playing Elevens!");
+    }
+
+    static void DisplayHand(List<Card> hand)  //display hand method
+    {
+        for (int i = 0; i < hand.Count; i++)
+        {
+            string rank = hand[i].Rank.ToString();
+            string suit = hand[i].Suit.ToString();
+            Console.WriteLine($"[{i}] {rank} of {suit}");
+        }
+    }
+}
